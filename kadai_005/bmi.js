23b53a6b7c5343544e20ad9eecdd03dcf873bbcb
sunvege <sunsunpokapoka0609@gmail.com>
2024-09-04 23:47:17 +0900

    kadai_005再提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,10 +1,12 @@
 //変数の宣言
 let bodyWeight;
 let bodyHeight;
+let bmi;
 
 //代入
 bodyWeight = 68;
 bodyHeight = 1.7;
+bmi = bodyWeight / (bodyHeight * bodyHeight);
 
 //コンソールへの出力
-console.log(bodyWeight / (bodyHeight * bodyHeight));
+console.log(bmi);
