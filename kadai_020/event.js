99be71bee23f39eec61d71658ad9cc90972935d2
sunvege <sunsunpokapoka0609@gmail.com>
2024-09-27 22:44:46 +0900

    kadai_021の提出

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,5 +2,11 @@ const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
+  //この一文が正解　text.textContent = 'ボタンをクリックしました';
+  const h2 = document.createElement('h2');
+  h2.textContent = 'ボタンをクリックしました';
+  text.appendChild(h2);
+  //text.remove();
+  
   text.textContent = 'ボタンをクリックしました';
 })
